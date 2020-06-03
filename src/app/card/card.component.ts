48e50b9bb23b858bd4c79c776da74a69ab6ef9f1
m0nq <forgetmenaught@gmail.com>
2020-06-03 00:27:39 -0700

    Generate and wire through card component

---
+++ src/app/card/card.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'cq-card',
+    templateUrl: './card.component.html',
+    styleUrls: ['./card.component.css']
+})
+export class CardComponent implements OnInit {
+
+    constructor() { }
+
+    ngOnInit(): void {
+    }
+}
